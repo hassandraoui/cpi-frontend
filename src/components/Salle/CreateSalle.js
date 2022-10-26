e13d64b3d10bdff71ad8587b479930462b64ffda
hassandraoui <114435542+hassandraoui@users.noreply.github.com>
2022-10-26 08:57:02 +0000

    Update CreateSalle.js

--- src/components/Salle/CreateSalle.js
+++ src/components/Salle/CreateSalle.js
@@ -1,7 +1,7 @@
 //import { Link } from "react-router-dom";
 
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const CreateSalle = (req,res) => {
