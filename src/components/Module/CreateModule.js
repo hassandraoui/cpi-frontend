9748e94d880ffe4eef5e7a151ef7ddb7e1a91f7e
hassandraoui <114435542+hassandraoui@users.noreply.github.com>
2022-10-26 08:54:05 +0000

    Update CreateModule.js

--- src/components/Module/CreateModule.js
+++ src/components/Module/CreateModule.js
@@ -1,7 +1,7 @@
 //import { Link } from "react-router-dom";
 
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const CreateModule = (req,res) => {
