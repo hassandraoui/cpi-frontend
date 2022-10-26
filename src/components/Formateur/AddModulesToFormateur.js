531042b905d2d5ac301544ae3cf377420b501bb9
hassandraoui <114435542+hassandraoui@users.noreply.github.com>
2022-10-26 08:33:27 +0000

    Update AddModulesToFormateur.js

--- src/components/Formateur/AddModulesToFormateur.js
+++ src/components/Formateur/AddModulesToFormateur.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const AddModulesToFormateur = () => {
 
